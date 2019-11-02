9583da1b5bac823a332d8cefc70e05503c35125f
bingoogolapple <bingoogolapple@gmail.com>
2019-11-02 17:03:48 +0800

    fix #298 fix #384 fix #422 fix #468 fix #121

--- publishToJcenter.sh
+++ publishToJcenter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash +x
 
+sed -i -e "s/\/\/ classpath 'com.novoda:bintray-release:0.8.1'/classpath 'com.novoda:bintray-release:0.8.1'/" build.gradle
+
 ./gradlew :qrcodecore:clean :qrcodecore:build :qrcodecore:bintrayUpload -PpublishAar
 ./gradlew :zxing:clean :zxing:build :zxing:bintrayUpload -PpublishAar
 ./gradlew :zbar:clean :zbar:build :zbar:bintrayUpload -PpublishAar
+
+sed -i -e "s/classpath 'com.novoda:bintray-release:0.8.1'/\/\/ classpath 'com.novoda:bintray-release:0.8.1'/" build.gradle
